2d446418a53e716c6c8e9ec99de585a49d4c7586
Chad Bowler <chadbowler@gmail.com>
2023-06-09 18:56:20 -0600

    fleshed out license rendering functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,67 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   switch (license) {
-    case value:
-      
-      break;
-      case value:
-      
-      break;
-      case value:
-      
-      break;
-  
+    case 'Apache License 2.0':
+      return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+     
+    case 'BSD 2-Clause':
+      return 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg';
+
+    case 'BSD 3-Clause':
+      return 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg';
+
+    case 'GNU GPLv2':
+      return 'https://img.shields.io/badge/License-GPL_v2-blue.svg';
+
+    case 'GNU AGPLv3':
+      return 'https://img.shields.io/badge/License-AGPL_v3-blue.svg';
+
+    case 'GNU GPLv3':
+      return 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+
+    case 'GNU LGPLv3':
+      return 'https://img.shields.io/badge/License-LGPL_v3-blue.svg';
+
+    case 'MIT License':
+      return 'https://img.shields.io/badge/License-MIT-yellow.svg';
+
+    case 'The Unlicense':
+      return 'https://img.shields.io/badge/license-Unlicense-blue.svg';
+    
     default:
       return "";
   }
 }
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
 
   switch (license) {
-    case value:
-      
-      break;
-      case value:
-      
-      break;
-      case value:
-      
-      break;
-  
+    case 'Apache License 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+     
+    case 'BSD 2-Clause':
+      return 'https://opensource.org/licenses/BSD-2-Clause';
+
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+
+    case 'GNU GPLv2':
+      return 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
+
+    case 'GNU AGPLv3':
+      return 'https://www.gnu.org/licenses/agpl-3.0';
+
+    case 'GNU GPLv3':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+
+    case 'GNU LGPLv3':
+      return 'https://www.gnu.org/licenses/lgpl-3.0';
+
+    case 'MIT License':
+      return 'https://opensource.org/licenses/MIT';
+
+    case 'The Unlicense':
+      return 'http://unlicense.org/';
+    
     default:
       return "";
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
-
-  switch (license) {
-    case value:
-      
-      break;
-      case value:
-      
-      break;
-      case value:
-      
-      break;
-  
-    default:
+    if (license !== '') {
+      return license;
+    } else {
       return "";
-  }
-}
+    };
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log('here');
-  renderLicenseBadge(data.license);
-  renderLicenseLink(data.licence);
-  renderLicenseSection(data.license);
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.licence);
+  const licenseSection = renderLicenseSection(data.license);
   return `# ${data.title}
 
 `;
